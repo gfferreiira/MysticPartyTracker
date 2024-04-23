df3edc46bc02a09a1b0806b66a3da9bcf2dc8797
gfferreiira <arzhun7381@gmail.com>
2024-04-22 22:46:02 -0300

    First Commit

---
+++ MysticPartyTracker/Model/Dice.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysticPartyTracker.Model
+{
+    public class Dice
+    {
+        public Dice()
+        {
+        }
+
+        public Dice(int side)
+        {
+            Side = side;
+        }
+       
+
+        public int Side { get; set; }
+        public int Rolar()
+        {
+            //Retorna o valor da rolagem do dado.
+            return new Random().Next(1, Side + 1);
+        }
+
+    }
+}
---
+++ MysticPartyTracker/View/DiceView.xaml.cs
@@ -0,0 +1,36 @@
+namespace MysticPartyTracker.View;
+using MysticPartyTracker.Model;
+
+public partial class DiceView : ContentPage
+{
+	public DiceView()
+	{
+		InitializeComponent();
+
+	}
+    private void DiceBtn_Clicked(object sender, EventArgs e)
+    {
+        //Verificar quantidade de dados pelo picker
+        int dices = Convert.ToInt32(DicesPicker.SelectedIndex);
+
+        //Instanciação de um novo objeto dice do tipo Dice, através do construtor Dice();
+
+        int sides = Convert.ToInt32(SidesPicker.SelectedItem);
+        Dice dice = new Dice(sides);
+        DiceResult.Text = Convert.ToString(dice.Rolar());
+       
+
+
+    }
+    private void DiceTypePicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DiceTypeImage.Source = $"d{SidesPicker.SelectedItem.ToString()}.png";
+    }
+    private void DicePicker_SelectedIndexChanged(Object sender, EventArgs e)
+    {
+        
+    }
+
+
+
+}
