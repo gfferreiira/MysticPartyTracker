5ff117d2417449bc4be457cfb98e10d541056c59
gfferreiira <arzhun7381@gmail.com>
2024-04-29 19:54:28 -0300

    Utilização da rolagem de dados com condição for

--- MysticPartyTracker/View/DiceView.xaml.cs
+++ MysticPartyTracker/View/DiceView.xaml.cs
@@ -6,33 +6,24 @@ public partial class DiceView : ContentPage
 	public DiceView()
 	{
 		InitializeComponent();
+        
        
     }
     private void DiceBtn_Clicked(object sender, EventArgs e)
     {
+        DiceResult.Text = string.Empty;
+
         //Verificar quantidade de dados pelo picker
-        int dices = Convert.ToInt32(DicesPicker.SelectedIndex);
+        int dices = Convert.ToInt32(DicesPicker.SelectedItem);
         //Instanciação de um novo objeto dice do tipo Dice, através do construtor Dice();
 
         int sides = Convert.ToInt32(SidesPicker.SelectedItem);
-        Dice dice = new Dice(sides);
-        DiceResult.Text = Convert.ToString(dice.Rolar());
-
-        if (DicesPicker.SelectedIndex == 1)
-        {
-            Dice dicetwo = new Dice(sides);    
-            DiceTwoResult.Text = Convert.ToString(dice.Rolar());
+        for (int i = 0; i < dices; i++) {
+            
+            DiceResult.Text = DiceResult.Text + Convert.ToString(new Dice(sides).Rolar()) + "   ";
         }
-        if (DicesPicker.SelectedIndex == 2)
-        {
-            Dice dicetwo = new Dice(sides);
-            Dice dicethree = new Dice(sides);
-            DiceTwoResult.Text = Convert.ToString(dice.Rolar());
-            DiceThreeResult.Text = Convert.ToString(dice.Rolar());
 
-
-
-        }
+        
 
 
     }
