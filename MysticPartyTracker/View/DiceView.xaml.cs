bd0c98b9cb1beabb77fd013c67a09b78f33b7440
gfferreiira <arzhun7381@gmail.com>
2024-05-06 19:51:57 -0300

    Teste

--- MysticPartyTracker/View/DiceView.xaml.cs
+++ MysticPartyTracker/View/DiceView.xaml.cs
@@ -1,11 +1,13 @@
 namespace MysticPartyTracker.View;
 using MysticPartyTracker.Model;
+using MysticPartyTracker.ViewModel;
 
 public partial class DiceView : ContentPage
 {
 	public DiceView()
 	{
 		InitializeComponent();
+        BindingContext = new DiceViewModel();
         
        
     }
